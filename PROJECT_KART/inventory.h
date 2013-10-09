1272a0ec7e5c27bbed3e33fadddda5efda4d0f0a
Tiago elias <merovigiam@gmail.com>
2013-10-09 10:46:20 +0100

    missing code

--- PROJECT_KART/inventory.h
+++ PROJECT_KART/inventory.h
@@ -1,10 +1,18 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
+#include <vector>
+#include "item.h"
+using namespace std;
+
 class Inventory
 {
+private:
+    vector<Item> inv;
+
 public:
     Inventory();
+
 };
 
 #endif // INVENTORY_H
