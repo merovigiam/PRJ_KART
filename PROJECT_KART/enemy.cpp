e9bad27d8ca81cf5d397a34fc4609d060ea10a60
TheIceMageX50 <palkiax50@topmail.e>
2013-10-14 16:21:45 +0100

    commiting Enemy.h and .cpp
    The code seems mostly okay except for the class' destructor I am getting
    compilation errors that I can' figure out; something about pointers
    being expected instead of string/int/etc Not too sure about that as we
    haven't covered pointers yet.

--- PROJECT_KART/enemy.cpp
+++ PROJECT_KART/enemy.cpp
@@ -1,5 +1,43 @@
 #include "enemy.h"
 
-Enemy::Enemy()
-{
-}
+    Enemy::Enemy(string name, int hitPoints, int armour, int power)
+    {
+        this->name = name ;
+        this->hitPoints = hitPoints ;
+        this->armour = armour ;
+        this->power = power ;
+    }
+
+    string Enemy::getName()
+    {
+        return name ;
+    }
+
+    void Enemy::setHitPoints(int hitPoints)
+    {
+        this->hitPoints = hitPoints ;
+    }
+
+    int Enemy::getHitPoints()
+    {
+        return hitPoints ;
+    }
+
+    int Enemy::getArmour()
+    {
+        return armour ;
+    }
+
+    int Enemy::getPower()
+    {
+        return power ;
+    }
+
+    Enemy::~Enemy()
+    {
+        delete(name) ;
+        delete hitPoints ;
+        delete armour ;
+        delete power ;
+        delete this ;
+    }
--- PROJECT_KART/enemy.h
+++ PROJECT_KART/enemy.h
@@ -1,10 +1,24 @@
 #ifndef ENEMY_H
 #define ENEMY_H
+#include<string>
+using namespace std ;
 
 class Enemy
 {
+
+private:
+    string name ;
+    int hitPoints ;
+    int armour ;
+    int power ;
 public:
-    Enemy();
+    Enemy(string name, int hitPoints = 100, int armour = 0 , int power = 10) ;
+    ~Enemy() ;
+    string getName() ;
+    void setHitPoints(int hitPoints) ;
+    int getHitPoints() ;
+    int getArmour() ;
+    int getPower() ;
 };
 
 #endif // ENEMY_H
